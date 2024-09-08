4c9a10f630c16d70d1a47c4373b436b863ee1ca0
muhammadIkramUlHaq <ikramulhaq1992@gmail.com>
2024-09-08 23:37:04 +0200

    Add and organize the folder structure on frontend

---
+++ fyndbox-frontend/src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,26 @@
+import { Button, Box } from "@mui/material";
+import { FC } from "react";
+import { StyledContainer, StyledTypography } from "./LandingPageStyles";
+
+const LandingPage: FC = () => {
+  return (
+    <StyledContainer maxWidth="md">
+      <Box textAlign="center">
+        <StyledTypography variant="h3" gutterBottom mb={8}>
+          Välkommen till FyndBox
+        </StyledTypography>
+        <StyledTypography variant="h6" gutterBottom mb={10}>
+          Vi erbjuder en effektiv och smidig lösning för att få fullständig
+          kontroll över ditt lager. Med vår webbplatform kan du enkelt
+          organisera och hantera dina inventarier genom att lägga till lådor och
+          ange bilder samt antal för varje objekt.
+        </StyledTypography>
+        <Button variant="contained" color="primary" href="#login">
+          Login
+        </Button>
+      </Box>
+    </StyledContainer>
+  );
+};
+
+export default LandingPage;
