faa2b68e9be1a568b7b8a73ea01a29cc91b23e70
Muhammad Ikram Ul Haq <ikramulhaq1992@gmail.com>
2024-09-17 14:19:32 +0200

    * #5: Added Login Page and styling
    * #5: Added styling for landing page

--- fyndbox-frontend/src/App.tsx
+++ fyndbox-frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 const App: FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
 
         {/* 
         <Route
