e610e4f8c37edadb9d4ac6f75c8f141699eda35e
muhammadIkramUlHaq <ikramulhaq1992@gmail.com>
2024-09-23 00:28:27 +0200

    #28: Added translation for landing page

--- shared/constants/index.ts
+++ shared/constants/index.ts
@@ -2,6 +2,10 @@ import en from "./i18n/en.json";
 import sv from "./i18n/sv.json";
 
 export const translations = {
-  en,
-  sv,
+  en: {
+    translation: en,
+  },
+  sv: {
+    translation: sv,
+  },
 };
