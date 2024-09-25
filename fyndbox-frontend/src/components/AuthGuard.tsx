8bfbe9cc31a1381f38fec90fc8b6b248c471d74f
muhammadIkramUlHaq <ikramulhaq1992@gmail.com>
2024-09-26 01:45:52 +0200

    #38: Added search text field

--- fyndbox-frontend/src/components/AuthGuard.tsx
+++ fyndbox-frontend/src/components/AuthGuard.tsx
@@ -1,6 +1,6 @@
-import { FC, ReactElement } from "react";
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../hooks/useAuth";
+import { FC, ReactElement } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
 
 const AuthGuard: FC<{ children: ReactElement }> = ({ children }) => {
   const { isAuthenticated } = useAuth();
--- fyndbox-frontend/src/pages/DashboardPage/DashboardPage.tsx
+++ fyndbox-frontend/src/pages/DashboardPage/DashboardPage.tsx
@@ -1,12 +1,16 @@
 import { FC } from 'react';
 import { FullPageContainer } from '../../styles/commonStyles';
 import TopBar from '../../components/TopBar/TopBar';
+import SearchField from '../../components/SearchField/SearchField';
 
 const DashboardPage: FC = () => {
   return (
     <>
       <TopBar />
-      <FullPageContainer></FullPageContainer>;
+      <FullPageContainer>
+        <SearchField />
+      </FullPageContainer>
+      ;
     </>
   );
 };
