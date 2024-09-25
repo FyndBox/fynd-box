caa0ec09453d50cc6018ef9037c1ad082aba5e0a
muhammadIkramUlHaq <ikramulhaq1992@gmail.com>
2024-09-25 02:19:20 +0200

    #31: Added ts ignore

--- fyndbox-backend/src/common/base.service.ts
+++ fyndbox-backend/src/common/base.service.ts
@@ -6,6 +6,8 @@ export abstract class BaseService {
   @Inject(REQUEST) protected request: Request;
 
   protected getLang(): string {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore: Ignore the 'property does not exist on type' error
     return this.request.language || 'en';
   }
 }
