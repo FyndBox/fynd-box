dedfd891d99f4b7c5ad2b86db68c5a1b4556725b
muhammadIkramUlHaq <ikramulhaq1992@gmail.com>
2024-09-26 22:05:38 +0200

    #38: Add loading state in AuthContext

--- fyndbox-frontend/src/components/AuthGuard.tsx
+++ fyndbox-frontend/src/components/AuthGuard.tsx
@@ -3,7 +3,11 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 
 const AuthGuard: FC<{ children: ReactElement }> = ({ children }) => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
