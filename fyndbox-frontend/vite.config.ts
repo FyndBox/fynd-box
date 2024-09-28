242911c4e80d8b95b5d583a5fac7168372afd36f
Muhammad Ikram Ul Haq <ikramulhaq1992@gmail.com>
2024-09-29 01:14:09 +0200

    Update vite.config.ts

--- fyndbox-frontend/vite.config.ts
+++ fyndbox-frontend/vite.config.ts
@@ -4,9 +4,12 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173, // Use Heroku's dynamic port, fallback to 5173 locally
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // Backend server URL
+        target: process.env.NODE_ENV === 'production' 
+          ? "https://fyndbox-cddc55cc8183.herokuapp.com" // Backend URL in production
+          : "http://localhost:3000", // Backend server URL in development
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
