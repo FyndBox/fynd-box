f5392ed0aafeb0f3fb53007965ddf156aa49472b
muhammadIkramUlHaq <ikramulhaq1992@gmail.com>
2024-10-05 01:35:54 +0200

    #45: Enable CORS origin in backend for FE

--- fyndbox-backend/src/main.ts
+++ fyndbox-backend/src/main.ts
@@ -5,8 +5,17 @@ import { LanguageInterceptor } from './interceptors/language.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const isProduction = process.env.NODE_ENV === 'production';
+
+  app.enableCors({
+    origin: isProduction ? 'https://fyndbox.com' : 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // Enable credentials (if needed, e.g., for cookies or tokens)
+  });
+
   app.useGlobalInterceptors(new LanguageInterceptor());
   app.useGlobalPipes(new ValidationPipe());
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
